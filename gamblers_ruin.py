58e20f9311ecd60674508a9fe1f430e04e4cc8cb
BernhardKonrad <bernhard.konrad@gmail.com>
2015-04-09 01:29:26 -0700

    Keeps history of all runs.

--- gamblers_ruin.py
+++ gamblers_ruin.py
@@ -1,15 +1,15 @@
 # Simulate gamblers ruin
 from random import random
 
-moneyA = 10
-moneyB = moneyA
+moneyA = [10]
+moneyB = [10]
 
-while moneyA > 0 and moneyB > 0:
+while moneyA[-1] > 0 and moneyB[-1] > 0:
     if random() < 0.5:
-        moneyA += 1
-        moneyB -= 1
+        moneyA.append(moneyA[-1] + 1)
+        moneyB.append(moneyB[-1] - 1)
     else:
-        moneyA -= 1
-        moneyB += 1
+        moneyA.append(moneyA[-1] - 1)
+        moneyB.append(moneyB[-1] + 1)
 
 print moneyA, moneyB
