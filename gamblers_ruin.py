76579883b70eb3070d57535578427d05ffd7f78e
BernhardKonrad <bernhard.konrad@gmail.com>
2015-04-09 01:45:15 -0700

    Wraps the simulation into a function.

--- gamblers_ruin.py
+++ gamblers_ruin.py
@@ -1,14 +1,40 @@
 # Simulate gamblers ruin
 from random import random
 
-initial = 10
-moneyA = [initial]
 
-while moneyA[-1] > 0 and moneyA[-1] < 2 * initial:
-    if random() < 0.5:
-        moneyA.append(moneyA[-1] + 1)
+def simulate_one(initial, successA):
+    '''
+    Simulates gambler's ruin with
+        two players,
+        equal initial condition,
+        fixed success rate,
+        fixed bet size (1 unit)
+
+    pre:
+        initial: initial starting bankroll for either player.
+        successA: (constant) success rate for player A at each turn.
+
+    post:
+        winner: 1 if player A wins, 0 otherwise
+        moneyA: history of bankroll of player A (list)
+        moneyB: history of bankroll of player B (list)
+    '''
+
+    moneyA = [initial]
+
+    while moneyA[-1] > 0 and moneyA[-1] < 2 * initial:
+        if random() < successA:
+            moneyA.append(moneyA[-1] + 1)
+        else:
+            moneyA.append(moneyA[-1] - 1)
+
+    moneyB = [2 * initial - m for m in moneyA]
+    if moneyA[-1] > 0:
+        winner = 1
     else:
-        moneyA.append(moneyA[-1] - 1)
+        winner = 0
 
-moneyB = [2 * initial - m for m in moneyA]
-print moneyA, moneyB
+    return winner, moneyA, moneyB
+
+print(simulate_one(5, 0.5))
+print(simulate_one(10, 0.1))
